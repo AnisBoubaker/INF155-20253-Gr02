be22b03eaa64c4826419834f5431b8584cd20b19
Anis Boubaker <anis@boubaker.ca>
2025-10-02 12:07:02 -0400

    Cours 04

--- COURS04/Damier/main.c
+++ COURS04/Damier/main.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+void afficher_damier(int nb_lignes, int nb_colonnes);
+
+int main(void) {
+    afficher_damier(5,4);
+
+    return EXIT_SUCCESS;
+}
+
+
+void afficher_damier(int nb_lignes, int nb_colonnes){
+
+    for(int i=0; i<nb_lignes; i++)
+    {
+        for(int j=0; j<nb_colonnes; j++)
+        {
+            //if((i%2==0 && j%2==0) || (i%2!=0 && j%2!=0))
+            if(  (i+j)%2 == 0  )
+            {
+                printf("#");
+            }
+            else
+            {
+                printf(".");
+            }
+        }
+        printf("\n");
+    }
 }
