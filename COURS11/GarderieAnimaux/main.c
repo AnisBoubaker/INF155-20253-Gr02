7d01f5e431b35fe3cfea1d922673fe352bb7649c
Anis Boubaker <anis@boubaker.ca>
2025-11-27 10:02:29 -0500

    Cours 11

--- COURS11/GarderieAnimaux/main.c
+++ COURS11/GarderieAnimaux/main.c
@@ -1,6 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct personne{
+    char* nom;
+    char* prenom;
+    char telephone[20];
+} t_personne;
+
+typedef struct date{
+    int jour;
+    int mois;
+    int annee;
+} t_date;
+
+typedef struct animal{
+    char* nom;
+    char espece[100];
+    char race[100];
+    t_date date_naiss;
+    t_personne proprietaire;
+} t_animal;
+
 
 int main() {
-    printf("Hello, World!\n");
+    t_personne p1;
+    t_animal* a1;
+
+    p1.nom = (char*)malloc(sizeof(char)*(strlen("Boulanger")+1));
+    strcpy(p1.nom , "Boulanger");
+    p1.prenom = (char*)malloc(sizeof(char)*(strlen("Romy")+1));
+    strcpy(p1.prenom , "Romy");
+    strcpy(p1.telephone, "514-345-8967");
+
+    //printf("La personne: %s %s, Telephone: %s\n",
+    //       p1.nom, p1.prenom, p1.telephone);
+
+    a1 = (t_animal*)malloc(sizeof(t_animal));
+    a1->nom = (char*)malloc(sizeof(char)*(strlen("Choupi")+1));
+    strcpy(a1->nom, "Choupi");
+    strcpy(a1->espece, "Chien");
+    strcpy(a1->race, "Golden");
+    a1->date_naiss.annee = 2021;
+    a1->date_naiss.mois = 1;
+    a1->date_naiss.jour = 20;
+    a1->proprietaire = p1;
+
+    printf("Nom: %s, Espece: %s, Race: %s\nDate de naiss: %d-%d-%d\n",
+           a1->nom, a1->espece, a1->race, a1->date_naiss.jour,
+           a1->date_naiss.mois, a1->date_naiss.annee);
+    printf("Propriétaire: %s %s, Tel: %s\n",
+           a1->proprietaire.prenom, a1->proprietaire.nom,
+           a1->proprietaire.telephone);
+
     return 0;
 }
